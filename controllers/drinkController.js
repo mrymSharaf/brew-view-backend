f4e0fbdcd0a0c78a7f6eeba64ee6984354450925
Mariam Sharaf <maryamshaider25@gmail.com>
2025-08-16 15:08:35 +0300

    add all drinks and drink details routes

--- controllers/drinkController.js
+++ controllers/drinkController.js
@@ -4,12 +4,44 @@ async function createDrink(req, res) {
     try {
         const newDrink = await Drink.create(req.body)
         res.status(201).json(newDrink)
-        
+
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+async function allDrinks(req, res) {
+    try {
+        const allDrinks = await Drink.find()
+
+        if (allDrinks) {
+            res.status(200).json(allDrinks)
+        } else {
+            res.status(204)
+        }
+
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+async function drinkDetails(req, res) {
+    try {
+        const drinkDetails = await Drink.findById(req.params.id)
+
+        if (drinkDetails) {
+            res.status(200).json(drinkDetails)
+        } else {
+            res.status(204)
+        }
+
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
 }
 
 module.exports = {
-    createDrink
+    createDrink,
+    allDrinks,
+    drinkDetails
 }
--- routes/drinkRoute.js
+++ routes/drinkRoute.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const drinkControllers = require('../controllers/drinkController')
 
 router.post('/new', drinkControllers.createDrink)
+router.get('/', drinkControllers.allDrinks)
+router.get('/:id', drinkControllers.drinkDetails)
 
 module.exports = router
