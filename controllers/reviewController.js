4a76a20c5b0c3e2be6963cdeb01253f13798c9ab
ZahraaMohd-c <202203702@stu.uob.edu.bh>
2025-08-16 13:26:20 +0300

    get , put reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,7 +1,38 @@
 const Review = require('../models/Reviews')
 
+async function createReview(req,res) {
+    try{
+        const newReview = await Review.create(req.body)
+        res.status(201).json(newReview)
 
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({error: error.message})
+    }
+
+}
+
+async function getAllReviews(req,res) {
+    try{
+        const allReviews = await Review.find()
+        if(allReviews.length){
+            res.status(200).json(allReviews)
+
+        }
+        else{
+            res.status(204)
+        }
+
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({error: error.message})
+    }
+}
 
 module.exports = {
-    
+    createReview,
+    getAllReviews
+
 }
--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const reviewController = require('../controllers/reviewController')
 
 
-
+router.post('/new',reviewController.createReview)
+router.get('/',reviewController.getAllReviews)
 
 module.exports = router
