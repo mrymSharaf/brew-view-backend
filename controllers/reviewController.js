f594186432e3181d9211a402cb8e4509f67c9cbb
ZahraaMohd-c <202203702@stu.uob.edu.bh>
2025-08-20 09:48:05 +0300

    reviews controller edit

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -15,7 +15,7 @@ async function createReview(req,res) {
 
 async function getAllReviews(req,res) {
     try{
-        const allReviews = await Review.find()
+        const allReviews = await Review.find().populate('user')
         if(allReviews.length){
             res.status(200).json(allReviews)
 
