643a0996fa86bb35b5b02bf8a4681611b49414ec
Mariam Sharaf <maryamshaider25@gmail.com>
2025-08-20 14:21:55 +0300

    clean up routes and remove validateRole middelware

--- routes/cafeRoute.js
+++ routes/cafeRoute.js
@@ -2,26 +2,14 @@ const express = require('express')
 const router = express.Router()
 const cafeControllers = require('../controllers/cafeController')
 const secureRoute = require('../middleware/secureRoute')
-const { cafeOnly } = require('../middleware/validateRole')
 
 const { cloudinary, multerSetup } = require('../config/Cloudinary')
 const upload = multerSetup()
 
-router.post('/new',
-    // cafeOnly,
-    // secureRoute,
-    upload.single("cafeImage"),
-    cafeControllers.createCafe)
+router.post('/new', secureRoute, upload.single("cafeImage"), cafeControllers.createCafe)
 router.get('/', cafeControllers.allCafes)
 router.get('/:id', cafeControllers.cafeDetails)
-router.put('/:id',
-    // cafeOnly,
-    // secureRoute,
-    upload.single("cafeImage"),
-    cafeControllers.updateCafe)
-router.delete('/:id',
-    // cafeOnly,
-    // secureRoute,
-    cafeControllers.deleteCafe)
+router.put('/:id', secureRoute, upload.single("cafeImage"), cafeControllers.updateCafe)
+router.delete('/:id', secureRoute, cafeControllers.deleteCafe)
 
 module.exports = router
--- routes/drinkRoute.js
+++ routes/drinkRoute.js
@@ -2,26 +2,14 @@ const express = require('express')
 const router = express.Router()
 const drinkControllers = require('../controllers/drinkController')
 const secureRoute = require('../middleware/secureRoute')
-const { cafeOnly } = require('../middleware/validateRole')
 
 const { cloudinary, multerSetup } = require('../config/Cloudinary')
 const upload = multerSetup()
 
-router.post('/new',
-    // cafeOnly,
-    // secureRoute,
-    upload.single("drinkImage"),
-    drinkControllers.createDrink)
+router.post('/new', secureRoute, upload.single("drinkImage"), drinkControllers.createDrink)
 router.get('/', drinkControllers.allDrinks)
 router.get('/:id', drinkControllers.drinkDetails)
-router.put('/:id',
-    // cafeOnly,
-    // secureRoute,
-    upload.single("drinkImage"),
-    drinkControllers.updateDrink)
-router.delete('/:id',
-    // cafeOnly,
-    // secureRoute,
-    drinkControllers.deleteDrink)
+router.put('/:id', secureRoute, upload.single("drinkImage"), drinkControllers.updateDrink)
+router.delete('/:id', secureRoute, drinkControllers.deleteDrink)
 
 module.exports = router
